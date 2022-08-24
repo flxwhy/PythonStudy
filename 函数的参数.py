ecef048d08823a8d9e5ee4c4288024e9524b814b
changsheng fu <909412615@qq.com>
2022-08-24 16:56:16 +0800

    feature:学习代码

--- 函数的参数.py
+++ 函数的参数.py
@@ -77,3 +77,42 @@ calc(*nums)
 
 def person(name, age, **kw):
     print('name:', name, 'age:', age, 'other:', kw)
+
+# 关键字参数，参数检查
+
+
+def person(name, age, **kw):
+    if 'city' in kw:
+        pass
+    if 'job' in kw:
+        pass
+    print('name:', name, 'age:', age, 'other:', kw)
+
+# 如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。这种方式定义的函数如下：
+# 和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+# 这种参数多了也不行 少了也不行不算动态参数
+
+
+def person(name, age, *, city, job):
+    print(name, age, city, job)
+
+
+# 如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了
+
+# 命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错：
+def person(name, age, *args, city, job):
+    print(name, age, args, city, job)
+
+# 使用命名关键字参数时，要特别注意，如果没有可变参数，就必须加一个*作为特殊分隔符。如果缺少*，Python解释器将无法识别位置参数和命名关键字参数：
+
+
+# 练习
+def mul(x, y):
+    return x * y
+
+#答
+def mul(*x):
+    res = 1
+    for cal in x:
+        res *= cal
+    return res
