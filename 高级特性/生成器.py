4a055b66683d07bb529cdacfd8a94dcbcaa54922
e <git@email.com>
2022-08-25 22:13:56 +0800

    feature:编辑

--- 高级特性/生成器.py
+++ 高级特性/生成器.py
@@ -40,3 +40,48 @@ def fib(max):
 
 
 fib(6)
+
+# 仔细观察，可以看出，fib函数实际上是定义了斐波拉契数列的推算规则，
+# 可以从第一个元素开始，推算出后续任意的元素，这种逻辑其实非常类似generator。
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a+b
+        n += 1
+
+# 这就是定义generator的另一种方法。如果一个函
+# 数定义中包含yield关键字，那么这个函数就不再是一个普通函数，
+# 而是一个generator函数，调用一个generator函数将返回一个generator：
+
+# 这里，最难理解的就是generator函数和普通函数的执行流程不一样。普通函数是顺序执行
+# 遇到return语句或者最后一行函数语句就返回。而变成generator的函数
+
+# 在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+
+# 举个简单的例子，定义一个generator函数，依次返回数字1，3，5：
+
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield (3)
+    print('step 3')
+    yield 5
+
+
+o = odd()
+next(o)
+
+# 可以看到，odd不是普通函数，而是generator函数
+# 在执行过程中，遇到yield就中断，下次又继续执行
+# 执行3次yield后，已经没有yield可以执行了，所以，第4次调用next(o)就报错。
+
+# 这样调用永远返回1 因为多个实例总是执行第一步
+next(odd())
+
+for n in fib(6):
+    print(n)
